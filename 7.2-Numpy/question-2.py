756d0a580d0a45d1b81ddbbeb090824d7708ec2f
Akbar Maxmanazarov <60185963+Akbar2998@users.noreply.github.com>
2022-12-31 12:53:31 +0100

    Create question-2.py

---
+++ 7.2-Numpy/question-2.py
@@ -0,0 +1,7 @@
+#Каким способом можно обратиться к элементу двумерного массива? Выберите верные варианты кода.
+ my_array[1][2]    +
+ my_array(1, 2)       
+ my_array[1],[2]
+ my_array[1, 2]    +
+ my_array[1 2] 
+ answers(2,5) #second and fifth lines are answers of the question
